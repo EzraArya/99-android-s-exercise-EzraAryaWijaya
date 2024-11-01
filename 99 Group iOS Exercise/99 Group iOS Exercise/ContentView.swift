7d1f972fac4dcfd17cbc01dfba1930e53607f2df
EzraArya <ezrawijaya10@gmail.com>
2024-11-01 18:10:38 +0700

    Initial Commit

---
+++ 99 Group iOS Exercise/99 Group iOS Exercise/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  99 Group iOS Exercise
+//
+//  Created by Ezra Arya Wijaya on 01/11/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 99 Group iOS Exercise/99 Group iOS Exercise/_9_Group_iOS_ExerciseApp.swift
@@ -0,0 +1,17 @@
+//
+//  _9_Group_iOS_ExerciseApp.swift
+//  99 Group iOS Exercise
+//
+//  Created by Ezra Arya Wijaya on 01/11/24.
+//
+
+import SwiftUI
+
+@main
+struct _9_Group_iOS_ExerciseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
