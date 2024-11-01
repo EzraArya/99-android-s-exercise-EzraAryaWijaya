c400157fb98c0d165f9150c3633b840ea48fe89a
EzraArya <ezrawijaya10@gmail.com>
2024-11-01 22:13:05 +0700

    feat: Add color profile from design

---
+++ 99 Group iOS Exercise/99 Group iOS Exercise/Extensions/Color+Extension.swift
@@ -0,0 +1,13 @@
+//
+//  Color+extension.swift
+//  99 Group iOS Exercise
+//
+//  Created by Ezra Arya Wijaya on 01/11/24.
+//
+import Foundation
+import SwiftUICore
+
+extension Color {
+    static let primary = Color("Primary")
+    static let secondary = Color("Secondary")
+}
