0a6d3e61d0f42b362f46f5c156f0cf5516191d45
EzraArya <ezrawijaya10@gmail.com>
2024-11-04 13:50:59 +0700

    Feat: Add Dependencies.swift for Factory DI

---
+++ 99 Group iOS Exercise/99 Group iOS Exercise/DI/Dependencies.swift
@@ -0,0 +1,15 @@
+//
+//  Dependencies.swift
+//  99 Group iOS Exercise
+//
+//  Created by Ezra Arya Wijaya on 04/11/24.
+//
+
+import Factory
+
+extension Container {
+    var apiManager: Factory<APIManagerProtocol> {
+        self { APIManager() }
+            .singleton
+    }
+}
