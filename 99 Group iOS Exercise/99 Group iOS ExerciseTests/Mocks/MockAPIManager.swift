802f55d293bdef874fe6babf25b488845048b1df
EzraArya <ezrawijaya10@gmail.com>
2024-11-06 14:05:46 +0700

    Feat: Add MockAPIManager and MockMapsManager for testing

--- 99 Group iOS Exercise/99 Group iOS ExerciseTests/Mocks/MockAPIManager.swift
+++ 99 Group iOS Exercise/99 Group iOS ExerciseTests/Mocks/MockAPIManager.swift
@@ -5,14 +5,59 @@
 //  Created by Ezra Arya Wijaya on 05/11/24.
 //
 
-import Testing
+import XCTest
 @testable import _9_Group_iOS_Exercise
 import Factory
 
-struct MocksTest {
+class MockAPIManager: APIManagerProtocol {
+    var shouldReturnError = false
 
-    @Test func <#test function name#>() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+     func fetchPropertyList() async throws -> [Property] {
+         if shouldReturnError {
+             throw NSError(domain: "", code: -1, userInfo: nil)
+         }
+         return [
+            Property(
+            address: Address(
+                district: "Mock District",
+                streetName: "Mock Street"),
+            attributes: Attributes(
+                areaSize: 1,
+                bathrooms: 1,
+                bedrooms: 1,
+                price: 1),
+            category: "Mock Category",
+            completedAt: "Mock",
+            id: 1,
+            photo: "https://www.moldexrealty.ph/house-models/",
+            projectName: "Mock Project Name",
+            tenure: 1)]
+     }
+     
+     func fetchPropertyDetail(id: Int) async throws -> PropertyDetail {
+         if shouldReturnError {
+             throw NSError(domain: "", code: -1, userInfo: nil)
+         }
+         return PropertyDetail(
+            address: DetailAddress(
+                mapCoordinates: MapCoordinates(
+                    lat: 1,
+                    lng: 1),
+                subtitle: "Mock Subtitle",
+                title: "Mock Title"),
+            attributes: Attributes(
+                areaSize: 1,
+                bathrooms: 1,
+                bedrooms: 1,
+                price: 1),
+            description: "Mock Description",
+            id: 1,
+            photo: "https://www.moldexrealty.ph/house-models/",
+            projectName: "Mock Project Name",
+            propertyDetails: [
+                propertyDetails(
+                    label: "Mock Label",
+                    text: "Mock Text")])
+     }
+    
 }
